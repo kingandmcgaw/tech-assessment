0cbc773e75e8ba8481a280323584195c38d099e6
Tom Fryer <fryertom@gmail.com>
2023-10-11 12:34:04 +0100

    Add Answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_11_113230) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_11_113342) do
+  create_table "answers", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "question_id", null: false
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "text"
@@ -25,5 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_11_113230) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
 end
