da4adf18562ae5f8441345b2f6592f4c07e836f4
Tom Fryer <fryertom@gmail.com>
2023-10-11 12:33:08 +0100

    Add Question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_11_111827) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_11_113230) do
+  create_table "questions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "questions", "users"
 end
